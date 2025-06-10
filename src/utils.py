f3b675cbe09da51a388cc8aaba8b03f63e5bc572
[Nicholas-Tjandra-Risk] <[nicholas.tjandra@indodanafinance.co.id]>
2025-06-10 15:08:43 +0700

    app flask

--- src/utils.py
+++ src/utils.py
@@ -7,6 +7,7 @@ from src.exception import CustomException
 import dill
 from sklearn.metrics import r2_score
 from sklearn.model_selection import GridSearchCV
+import pickle
 
 def save_object(file_path,obj):
     try:
@@ -38,4 +39,11 @@ def evaluate_model (X_train,y_train,X_test,y_test,models, params):
         return report
     except Exception as e:
         CustomException(e,sys)
-        pass    
+        pass   
+def load_object(file_path):
+    try:
+        with open(file_path, "rb") as file_obj:
+            return pickle.load(file_obj)
+
+    except Exception as e:
+        raise CustomException(e, sys)
